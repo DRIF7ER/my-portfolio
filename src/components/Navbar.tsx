9cdb2257d1c8a903dea8699b5b0e5763a3760ae7
Jim Macur <jim.macur@gmail.com>
2024-12-14 15:56:07 -0700

    feat: adds new font and animation to name in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,12 +5,21 @@ const Navbar: React.FC = () => {
     <nav className="fixed top-0 left-0 w-full bg-sky-500 shadow-md z-50" role="navigation" aria-label="Main Navigation">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <div className="flex items-center">
-            <h1 className="text-xl font-bold text-white">Jim Macur</h1>
+          <div className="flex items-center group">
+            <a
+              href="#home"
+              className="relative text-3xl font-bold text-white font-lobster transition-transform transform hover:scale-110 hover:text-orange-300"
+            >
+            Jim Macur
+            <span className="absolute left-0 bottom-0 w-0 h-1 bg-orange-300 transition-all duration-300 group-hover:w-full"></span>
+            </a>
           </div>
           <div className="flex space-x-4">
             <a href="#home" className="text-white hover:text-blue-700" aria-label="Go to Home section">
               Home
+            </a>
+            <a href="#aboutMe" className="text-white hover:text-blue-700" aria-label="Go to About Me section">
+              About Me
             </a>
             <a href="#projects" className="text-white hover:text-blue-700" aria-label="Go to Projects section">
               Projects
@@ -20,9 +29,6 @@ const Navbar: React.FC = () => {
             </a>
             <a href="#education" className="text-white hover:text-blue-700" aria-label="Go to Education section">
               Education
-            </a>
-            <a href="#aboutMe" className="text-white hover:text-blue-700" aria-label="Go to About Me section">
-              About Me
             </a>
             <a href="#contact" className="text-white hover:text-blue-700" aria-label="Go to Contact section">
               Contact
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        lobster: ['Lobster', 'cursive'],
+        pacifico: ['Pacifico', 'cursive'],
+        orbitron: ['Orbitron', 'sans-serif'],
+        bungee: ['Bungee', 'cursive'],
+      },
+    },
   },
   plugins: [],
 }
